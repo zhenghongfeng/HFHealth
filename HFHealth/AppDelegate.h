4f7df9d2c365b6136338db57062807a7ab4d49f1
Coder_feng <835762882@qq.com>
2016-05-04 15:33:23 +0800

    Initial Commit

---
+++ HFHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFHealth
+//
+//  Created by zhf on 16/5/4.
+//  Copyright © 2016年 zhenghongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HFHealth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFHealth
+//
+//  Created by zhf on 16/5/4.
+//  Copyright © 2016年 zhenghongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
